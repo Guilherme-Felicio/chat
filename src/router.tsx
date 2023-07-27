bd9f92f5fd4f38ccf18347f6451d63d8cbcb9f6b
Guilherme Felicio <guifelicio7@gmail.com>
2023-07-27 00:12:41 -0300

    feat: adicionada nova rota de casatro

--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import Login from "./pages/login";
+import SignUp from "./pages/register";
 
 export const router = createBrowserRouter([
   {
     path: "/chat/",
     element: <Login />,
+  },
+
+  {
+    path: "/chat/sign-up",
+    element: <SignUp />,
   },
 ]);
