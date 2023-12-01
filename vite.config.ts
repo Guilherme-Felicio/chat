df75971de009744da2de5723cd156112a2dba6df
Guilherme Felicio <guifelicio7@gmail.com>
2023-12-01 14:30:30 -0300

    refactor: added .env config to vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  define: {
+    VITE_LOGIN_URL: process.env.VITE_LOGIN_URL,
+  },
 });
