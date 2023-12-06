3ee5f86941678db2d6dbd35b22f819a71b1e1f8b
Guilherme Felicio <guifelicio7@gmail.com>
2023-12-06 14:37:37 -0300

    add opacity option to global loading

--- src/components/global-loading/index.tsx
+++ src/components/global-loading/index.tsx
@@ -1,8 +1,10 @@
 import LoadingIcon from "../../assets/icons/loading-icon";
 
-const GlobalLoading = () => {
+const GlobalLoading = ({ opacity }: { opacity: number }) => {
   return (
-    <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-98 bg-white z-50">
+    <div
+      className={`fixed top-0 left-0 w-full h-full flex items-center justify-center bg-opacity-[${opacity}] bg-white z-50`}
+    >
       <div className=" p-8 rounded-lg text-center flex justify-center flex-col items-center">
         <LoadingIcon className="animate-bounce" width={50} height={50} />
         <p className="text-2xl font-bold text-dark-green">
